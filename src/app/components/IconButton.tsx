72bb6769188fa33479d1a55319bb797e83e9ae85
Alex Wang <ipoopfool@gmail.com>
2020-05-19 16:37:47 -0700

    create IconButton component to handle disabled state

---
+++ src/app/components/IconButton.tsx
@@ -0,0 +1,38 @@
+import * as React from "react";
+
+type Props = {
+  // refer to the documentation for options
+  // https://github.com/thomas-lowry/figma-plugin-ds#icon
+  iconName: string;
+  onClick: any; // TODO update
+  disabled: boolean;
+  className?: string;
+};
+
+export const IconButton = ({
+  iconName,
+  onClick,
+  disabled,
+  className,
+  ...otherProps
+}: Props) => {
+  if (disabled) {
+    return (
+      <div className="icon-button-disabled">
+        <div
+          className={`icon icon--grey ${iconName} ${
+            className != null ? className : ""
+          }`}
+        ></div>
+      </div>
+    );
+  }
+
+  return (
+    <div className="icon-button" onClick={onClick} {...otherProps}>
+      <div
+        className={`icon ${iconName} ${className != null ? className : ""}`}
+      ></div>
+    </div>
+  );
+};
